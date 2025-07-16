c618bedde38559adbe28f91030f0ce42a6d0a834
Sahil <sahilsharma2000dtc@gmail.com>
2025-07-16 21:06:08 +0530

    client test working fine

--- user-ws/index.test.ts
+++ user-ws/index.test.ts
@@ -1,27 +1,65 @@
-import {describe, test} from 'bun:test';
+import { describe, expect, test } from "bun:test";
 
-const BACKEND_URL = "ws://localhost:8080"
+const BACKEND_URL = "ws://localhost:8080";
 
-describe("Chat application" , () => {
-    test("Message sent form room 1 reaches another participant in room 1", () => {
-        // create 2 ws connection
-        const ws1 = new WebSocket(BACKEND_URL)
-        const ws2 = new WebSocket(BACKEND_URL);
+describe("Chat application", () => {
+  test("Message sent form room 1 reaches another participant in room 1", async () => {
+    // create 2 ws connection
+    const ws1 = new WebSocket(BACKEND_URL);
+    const ws2 = new WebSocket(BACKEND_URL);
 
-        // make sure the socktes are connected.
+    // make sure the socktes are connected.  we can achieve this by using promise, its like pausing the thread and then pasing the control
+    await new Promise<void>((resolve, reject) => {
+      let count = 0;
+      ws1.onopen = () => {
+        count++;
+        if (count === 2) {
+          resolve();
+        }
+      };
+      ws2.onopen = () => {
+        count++;
+        if (count === 2) {
+          resolve();
+        }
+      };
+    });
 
+    // the below code will only reun when the both socket connection establish. we have awited the promise to mimic that.
+    ws1.send(
+      JSON.stringify({
+        type: "join-room",
+        room: "Room 1",
+      })
+    );
 
-        ws1.send(JSON.stringify({
-            type: "join-room",
-            room: "Room 1"
-        }));
+    ws2.send(
+      JSON.stringify({
+        type: "join-room",
+        room: "Room 1",
+      })
+    );
 
-        ws2.send(JSON.stringify({
-            type:"join-room",
-            room: "Room 1"
-        }));
+    console.log("Hello i have reachers here")
 
-        
+    await new Promise<void>((resolve) => {
+      ws2.onmessage = ({data}) => {
+        console.log(data.toString());
+        const parsedData = JSON.parse(data);
+        expect(parsedData.type == "chat");
+        expect(parsedData.message == "Hi there");
+        resolve(); // resove only happen whent we recieved the message.
+      };
+
+      ws1.send(
+        JSON.stringify({
+          type: "chat",
+          room: "Room 1",
+          message: "Hi there",
+        })
+      );
+
 
     });
-})
+  });
+});
