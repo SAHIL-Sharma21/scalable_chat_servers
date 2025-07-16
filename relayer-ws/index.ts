f3b18352205d3a3bce4da595c1caf23b113284ad
Sahil <sahilsharma2000dtc@gmail.com>
2025-07-16 22:09:48 +0530

    relayer services is implemented

---
+++ relayer-ws/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Relayer websocket server in which our 2 webseockets connection are connected 
+ * and data from onw ws can be passed to another ws server
+ */
+
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// server
+const servers: WebSocket[] = [];
+
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", (err) => {
+    console.error("Error: ", err);
+  });
+  // new connection then push to servers;
+  servers.push(ws);
+
+  ws.on("message", (data: string) => {
+    servers.filter(sockets => sockets != ws).map(socket => {
+        socket.send(data);
+    })
+  })
+
+});
